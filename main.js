8de668e73e0e6edc28cebacdd412aaee386eb2f8
kavi <jvandco30@gmail.com>
2026-02-02 06:00:59 +0530

    3rd commit

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send({obj1:"Hello, MoviesArea API!"});
+});
+
+///CRUD functionality for movies
+app.get("/movies", (req, res) => {
+  res.send({obj1:"List of movies will be here."});
+});
+app.post("/movies", (req, res) => {
+  res.send();
+});
+app.put("/movie/:id", (req, res) => {
+  res.send();
+});
+app.delete("/movies/:id", (req, res) => {
+  res.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
