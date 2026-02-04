5c416e1eed99712f23b3b015d4507e336c3c41e7
kavi <jvandco30@gmail.com>
2026-02-04 09:24:27 +0530

    modified few files

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { MovieIndex, movieList, MovieCreate, MovieUpdate, MovieDelete } from "../controllers/movies.controller.js";
 //const router = express();
 const router = express.Router();
 
 ///CRUD functionality for movies
 router.get("/", MovieIndex);
-router.get("/",movieList);
+router.get("/list",movieList);
 router.post("/", MovieCreate);
 router.put("/:id",MovieUpdate);
 router.delete("/:id", MovieDelete);
